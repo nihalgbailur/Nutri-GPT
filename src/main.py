c77dfe2a85526d7c65efc58e8ee9c7a667893169
NIHAL G BAILUR <nihalgbailur@NIHALs-MacBook-Air.local>
2025-06-11 20:16:34 +0530

    added build index function

--- src/main.py
+++ src/main.py
@@ -12,4 +12,22 @@ from llama_index import SimpleDirectoryReader
 # Centralized context manager for LLM settings (model, embeddings, etc.)
 from llama_index import ServiceContext
 
+# load env variables
+load_dotenv()
 
+api_key=os.getenv("OPEN_API_KEY") 
+
+logger.add("logs/app.log",rotation="10 MB")
+service_context = ServiceContext.from_defaults(
+    llm=LlamaOpenAI(model="gpt-4")
+)
+# Build index
+def build_food_index(data_path: str = "data") -> VectorStoreIndex:
+    try:
+        documents = SimpleDirectoryReader(data_path).load_data()
+        index = VectorStoreIndex.from_documents(documents, service_context=service_context)
+        logger.info("Food knowledge index built successfully.")
+        return index
+    except Exception as e:
+        logger.error(f"Error building index: {str(e)}")
+        return None
